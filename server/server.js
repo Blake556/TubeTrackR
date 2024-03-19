e3738ec28c7bfa4ed441aac1d33fefb551faf486
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2024-03-19 09:59:29 -0700

    Search component built out search sent to express > google cloud > response > express > front end

--- server/server.js
+++ server/server.js
@@ -5,16 +5,16 @@ const PORT = 3050
 const axios = require('axios');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
+//This is To keep sensitive data hidden 
 require('dotenv').config()
 
 const APIkey = process.env.API_KEY
 const clientID = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
-// Pre me sending access token to front end route
 const redirectURL = 'http://localhost:3050/handleOAuthCallback';
-// const redirectURL = 'http://localhost:3000';
 
+
+//This just makes working with backend port and a seperate front end port and the exchange of data
 const corsOptions = {
   origin: 'http://localhost:3000',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -25,18 +25,20 @@ const corsOptions = {
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
+
 const oauth2Client = new google.auth.OAuth2(
   clientID,
   clientSecret,
   redirectURL
 );
-
+//The data that im requesting from users youtube accounts when they sign in using my app. specified in my goolgle cloud app
 const youtubeScopes = [
   'https://www.googleapis.com/auth/youtube.readonly',
   'https://www.googleapis.com/auth/youtube',
   'https://www.googleapis.com/auth/youtube.force-ssl',
 ];
 
+//The first route to be called to initiate the google Oauth proccess
 app.get('/authUrl', async (req, res) => {
     const frontendRedirectUrl = 'http://localhost:3050/handleOAuthCallback';
     const authUrl = oauth2Client.generateAuthUrl({
@@ -48,24 +50,23 @@ app.get('/authUrl', async (req, res) => {
     res.json({ url: authUrl });
 });
 
-
+// 'handleOAuthCallback'route will use res.json({ url: authUrl }); and extract the data using code variable. 
 app.get('/handleOAuthCallback', async (req, res) => {
   const code = req.query.code;
-  //console.log(`Received code: ${code}`);
-
   try {
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
-
-    //console.log('Tokens received:', tokens);
-
     // Call fetchLikedVideos and wait for it to resolve
     const likedVideos = await fetchLikedVideos(tokens);
-    
-    // Redirect to frontend with the access token and liked videos as query parameters
+
+     // encodeURIComponent will make the accesstoken formated so it cant be sent through a url. JSON.stringify will turn the obj or array into a string so it can be successfully sent
     const accessTokenParam = encodeURIComponent(tokens.access_token);
+    // encodeURIComponent will make likedVideos formated so it cant be sent through a url. JSON.stringify will turn the obj or array into a string so it can be successfully sent
     const likedVideosParam = encodeURIComponent(JSON.stringify(likedVideos));
+    
+    //The url that res.redirect will use with accessTokenParam and likedVideosParam will be sent to front end in url
     const redirectURLWithParams = `http://localhost:3000?accessToken=${accessTokenParam}&likedVideos=${likedVideosParam}`;
+    //Redirect to frontend with the access token and liked videos
     res.redirect(redirectURLWithParams);
   } catch (error) { 
     console.error('Error exchanging code for tokens:', error);
@@ -75,6 +76,29 @@ app.get('/handleOAuthCallback', async (req, res) => {
 
 
 
+// This route is called when form on the front-end is submitted
+app.post('/search', async (req, res) => {
+  try {
+    const { query } = req.body;
+    // Communicate with YouTube API to search for videos based on the query
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        key: APIkey,
+        q: query,
+        part: 'snippet',
+        maxResults: 50 
+      }
+    });
+    // Send back the search results to the frontend
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error searching:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+//Upon successful oAuth fetchLikedVideos will be called from '/handleOAuthCallback' route to fetch liked videos and send it when responding to front end
 async function fetchLikedVideos(tokens) {
   try {
     const accessToken = tokens.access_token;
@@ -109,67 +133,6 @@ async function fetchLikedVideos(tokens) {
   }
 }
 
-
-
-//   app.post('/fetchLikedVideos', async (req, res) => {
-//     const accessToken = req.body.accessToken;
-//     console.log('uno ', accessToken)
-//     try {
-//         // Use the access token to fetch liked videos
-//         const likedVideos = await fetchLikedVideos(accessToken);
-//         // Respond with the liked videos
-//         res.json({ likedVideos });
-//     } catch (error) {
-//         console.error('Error fetching liked videos:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
-
-// app.get('/authUrl', (req, res) => {
-//     const frontendRedirectUrl = 'http://localhost:3050/handleOAuthCallback';
-//     const authUrl = oauth2Client.generateAuthUrl({
-//       access_type: 'offline',
-//       scope: youtubeScopes,
-//       redirect_uri: frontendRedirectUrl,
-//     });
-//     console.log('DID YOU CALL ME')
-//     res.json({ url: authUrl });
-//   });
-
-
-/*
-async function fetchLikeVideos(oauth2Client) {
-  try {
-    const accessToken = oauth2Client.credentials.access_token;
-    const endpoint = 'https://www.googleapis.com/youtube/v3/videos';
-    const params = {
-      videoCategoryId: 10,
-      part: 'snippet',
-      myRating: 'like',
-      maxResults: 50,
-    };
-
-    const response = await axios.get(endpoint, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      params: params,
-    });
-
-    console.log(response.data || 'NOTHING');
-    return response.data.items.map((video) => {
-      return {
-        title: video.snippet.title,
-        thumbnail: video.snippet.thumbnails.default.url,
-      };
-    });
-  } catch (error) {
-    console.log('Error fetching liked videos', error);
-    throw error;
-  }
-}
-*/
 
 
 /*
